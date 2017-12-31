b2615d9f39595388ba1bd80d498bd94bb47d4693
eyoshimoriyama <eyoshimoriyama@gmail.com>
2017-12-30 20:21:36 -0800

    Changed urrlib import

--- app.py
+++ app.py
@@ -4,8 +4,8 @@ from twilio.twiml.messaging_response import MessagingResponse, Message
 from twilio.twiml.voice_response import Play, VoiceResponse
 from twilio.rest import Client
 from config import twilio_account_sid, twilio_auth_token, ms_api_key
-import os
-import urllib
+from urllib.parse import quote
+
  
 # Account SID and Auth Token from www.twilio.com/console
 client = Client(twilio_account_sid, twilio_auth_token)
